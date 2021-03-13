55525ee46e904a231e8ecc1ad57c9c40338a9260
QuratulainSajid712 <80091216+QuratulainSajid712@users.noreply.github.com>
2021-03-13 20:46:50 +0500

    Login_Page
    UI Interface Design in Flutter

--- main.dart
+++ main.dart
@@ -1,92 +1,153 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  )
+      );
 }
-class MyApp extends StatelessWidget{
+
+class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          CircleAvatar(
-            backgroundImage:AssetImage('images/IMG-20190329-WA0017.png') ,
-            backgroundColor: Colors.white,
-            radius: 100.0,
-          ),
-          Text(
-            'Qurat-Ul-Ain Sajid',
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 30.0,
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 400,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/background.png'),
+                          fit: BoxFit.fill
+                      )
+                  ),
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned(
+                        left: 30,
+                        width: 80,
+                        height: 200,
+                        child:  Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage('assets/images/light-1.png')
+                              )
+                          ),
+                        ),
+                        ),
 
-              ),
-          ),
-          Text(
-            'CIIT/FA17-BCS-075/VHR',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 15.0,
-              letterSpacing: 4.0,
+                      Positioned(
+                        left: 140,
+                        width: 80,
+                        height: 150,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage('assets/images/light-2.png')
+                              )
+                          ),
+                        ),
+                        ),
 
-            ),
-          ),
-          SizedBox(
-            width: 600.0,
-            height: 20.0,
-            child: Divider(
-                color:  Colors.white,
-                thickness: 2.0
-            ),
-          ),
-          Card(
-            color: Colors.white,
-            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-            child: ListTile(
-              leading: Icon(Icons.phone,
-                color: Colors.teal,
-                size: 30.0,
-              ),
-              title: Text(
-                '+923458790103',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
+                      Positioned(
+                        right: 40,
+                        top: 40,
+                        width: 80,
+                        height: 150,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage('assets/images/clock.png')
+                              )
+                          ),
+                        ),
+                      ),
+
+                      Positioned(
+                        child:  Container(
+                          margin: EdgeInsets.only(top: 50),
+                          child: Center(
+                            child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  ),
                 ),
-              ),
+                Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Column(
+                    children: <Widget>[Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Color.fromRGBO(143, 148, 251, .2),
+                                  blurRadius: 20.0,
+                                  offset: Offset(0, 10)
+                              )
+                            ]
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                  border: Border(bottom: BorderSide(color: Colors.grey[100]))
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Email or Phone number",
+                                    hintStyle: TextStyle(color: Colors.grey[400])
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Password",
+                                    hintStyle: TextStyle(color: Colors.grey[400])
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 30,),
+                       Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: LinearGradient(
+                                colors: [
+                                  Color.fromRGBO(143, 148, 251, 1),
+                                  Color.fromRGBO(143, 148, 251, .6),
+                                ]
+                            )
+                        ),
+                        child: Center(
+                          child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                        ),
+                      ),
+                      SizedBox(height: 70,),
+                       Text("Forgot Password?", style: TextStyle(color: Color.fromRGBO(143, 148, 251, 1)),),
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
-          Card(
-            color: Colors.white,
-            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-            child: ListTile(
-              leading: Icon(Icons.email,
-                color: Colors.teal,
-                size: 30.0,
-              ),
-
-              title: Text(
-                'quratulainsajid12@gmail.com',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-        ]
-        ),
-      ),
-      ),
+        )
     );
-
-}
+  }
 }
