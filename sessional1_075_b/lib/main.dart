3390b6a9c50fa3ea2b6a2c5b9817de8ae5705d9c
QuratulainSajid712 <80091216+QuratulainSajid712@users.noreply.github.com>
2021-03-19 20:29:28 +0500

    set background with image and added logo

--- sessional1_075_b/lib/main.dart
+++ sessional1_075_b/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -12,27 +11,47 @@ class MyApp extends StatelessWidget {
       home: new Scaffold(
         //Here you can set what ever background color you need.
         backgroundColor: Colors.black,
-        body:SingleChildScrollView(
-          child: Container(
-            height: 160.0,
-            child: Stack(
-              children: <Widget>[
-            Container(
-            color: Colors.lightBlueAccent,
-              width: 600,
-              height: 2000.0,
-              child: Center(
-                child: Text(
-                  "Home",
-                  style: TextStyle(color: Colors.white, fontSize: 18.0),
-                ),
-              ),
-            ),
-            ],
-          ),
-        ),
-        ),
+        body: SingleChildScrollView(
+        child: Container(
+        child: Column(
+        children: <Widget>[
+        Container(
+        height: 330,
+        decoration: BoxDecoration(
+        image: DecorationImage(
+        image: AssetImage('images/1.jpg'),
+    fit: BoxFit.fill
+    )
+    ),
+    child: Stack(
+    children: <Widget>[
+
+    Positioned(
+    left: 100,
+    width: 100,
+    height:300.0,
+    child:  Container(
+
+    decoration: BoxDecoration(
+    image: DecorationImage(
+      image: AssetImage('images/pic1.png',),
+    )
+    ),
+    ),
+    ),
+
+
+    ],
+    ),
+
+    ),
+      ],
+    ),
+    ),
+    ),
       ),
-    );
+
+          );
+
   }
 }
