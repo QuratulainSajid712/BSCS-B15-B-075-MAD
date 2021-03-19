a218c52ec6020728810fe7815d9219947455eed9
QuratulainSajid712 <80091216+QuratulainSajid712@users.noreply.github.com>
2021-03-19 23:23:40 +0500

    Added login containers
    Added Stack and then used positioned widget and set settle the whole screen using container and positioned widget

--- sessional1_075_b/lib/main.dart
+++ sessional1_075_b/lib/main.dart
@@ -9,29 +9,44 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: new Scaffold(
-        //Here you can set what ever background color you need.
-        backgroundColor: Colors.black,
+
+
         body: SingleChildScrollView(
         child: Container(
         child: Column(
         children: <Widget>[
         Container(
-        height: 330,
+        height: 900,
         decoration: BoxDecoration(
         image: DecorationImage(
         image: AssetImage('images/1.jpg'),
-    fit: BoxFit.fill
-    )
-    ),
+        fit: BoxFit.fill
+        )
+        ),
+
     child: Stack(
     children: <Widget>[
+      Positioned(
+        top: 10,
+right: 0.01,
+left: 0.01,
 
+        height:320.0,
+        child:  Container(
+
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/pic3.png',),
+              )
+          ),
+
+        ),
+      ),
     Positioned(
-    left: 100,
+    left: 200,
     width: 100,
     height:300.0,
     child:  Container(
-
     decoration: BoxDecoration(
     image: DecorationImage(
       image: AssetImage('images/pic1.png',),
@@ -40,14 +55,88 @@ class MyApp extends StatelessWidget {
     ),
     ),
 
+    Positioned(
+    left: 80,
+    top: 250,
+    child: Container(
+    height: 280,
+    width: 300,
+    margin: EdgeInsets.symmetric(horizontal: 20),
+  decoration: BoxDecoration(
+    color: Colors.white,
+boxShadow:[
+  BoxShadow(
+  color: Colors.black.withOpacity(0.3),
+    blurRadius: 10,
+  spreadRadius: 5,
+),
+],
 
+  ),
+
+      child: Column( children:[
+        SizedBox(height: 50,),
+        Container(
+        width: 250,
+
+        child: TextField(
+          decoration: InputDecoration(
+            labelText: "User Name",
+            suffixIcon: Icon(Icons.email, size:17,
+            ),
+          ),
+        ),
+      ),
+      Container(
+        width: 250,
+        child: TextField(
+          decoration: InputDecoration(
+
+            labelText: "Password",
+            suffixIcon: Icon(Icons.remove_red_eye, size:17,
+            ),
+          ),
+
+        ),
+      ),
+
+      SizedBox(height: 81,)    ,
+      GestureDetector(
+        child: Container(
+          alignment: Alignment.center,
+          width: 300,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xFFF06292),
+                Color(0xFFF48FB1),
+                Color(0xFFF87121),
+              ],
+
+            ),
+
+          ),
+          child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text("Get Started",textAlign: TextAlign.right, style:TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold,color: Colors.white,))
+          ),
+        )
+      ),
+],
+    ),
+        ),
+
+    ),
     ],
     ),
 
+          
+        ),
+        ],
     ),
-      ],
-    ),
-    ),
+      ),
     ),
       ),
 
