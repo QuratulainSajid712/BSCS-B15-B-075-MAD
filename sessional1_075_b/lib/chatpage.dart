83435dbfafa8240d3fe4b7b0a3765fd60b2f1a8f
QuratulainSajid712 <80091216+QuratulainSajid712@users.noreply.github.com>
2021-03-20 22:02:22 +0500

    Add files via upload

--- sessional1_075_b/lib/chatpage.dart
+++ sessional1_075_b/lib/chatpage.dart
@@ -7,7 +7,7 @@ class homepage extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp (
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
@@ -66,7 +66,93 @@ class homepage extends StatelessWidget {
                             )
                         ),
 
+
                       ),
+                      Positioned(
+                        top: 20,
+                        left: 100,
+                        child: Container(
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.notifications,
+                                color: Colors.white,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text('1 New Message',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 10,)),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 40,
+                        left: 80,
+                        width: 80,
+                        height:150.0,
+                        child:  Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage('images/6.png',),
+                              )
+                          ),
+                        ),
+                      ),
+
+                      Container(
+                        height: 80,
+                        margin: EdgeInsets.only(left: 25, top: 25, right: 25),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Container(
+                                width: 113,
+                                height: 25,
+                                margin: EdgeInsets.only(top: 55),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 12,
+                                      height: 8,
+                                      margin: EdgeInsets.only(left: 7),
+                                      child: Image.asset(
+                                        "images/5.png",
+                                        fit: BoxFit.none,
+                                      ),
+                                    ),
+                                    Spacer(),
+                                    Align(
+                                      alignment: Alignment.topLeft,
+                                      child: Container(
+                                        width: 83,
+                                        margin: EdgeInsets.only(top: 6),
+                                        child: Text(
+                                          "RECENT CHATS",
+                                          textAlign: TextAlign.left,
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontFamily: "",
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 10,
+                                            letterSpacing: 1,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+
 
                     ],
                   ),
