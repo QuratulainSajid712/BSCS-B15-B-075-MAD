2e159b627f4bc999022176f78a3c5648df555f96
QuratulainSajid712 <80091216+QuratulainSajid712@users.noreply.github.com>
2021-03-20 13:21:14 +0500

    added background and container and profile phote

--- sessional1_075_b/lib/profileinfo.dart
+++ sessional1_075_b/lib/profileinfo.dart
@@ -1,54 +1,119 @@
 import 'package:flutter/material.dart';
 
-/*void main() {
+void main() {
   runApp(profilepage());
-}*/
+}
+
 class profilepage extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: SingleChildScrollView(
-        child: Container(
-        child: Column(
-        children: <Widget>[
-        Container(
-        height: 900,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('images/1.jpg'),
-                fit: BoxFit.fill
+        appBar: AppBar(
+
+          centerTitle: true,
+          title: Text(
+            'Profile',style: TextStyle(fontSize:15.0,color: Colors.black),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                // do something
+              },
             )
-        ),
-
-        child: Stack(
-          children: <Widget>[
-          Positioned(
-          top: 10,
-          right: 0.01,
-          left: 0.01,
-
-          height:320.0,
-          child:  Container(
-
+          ],
+          flexibleSpace: Container(
             decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('images/pic3.png',),
-                )
+              gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: <Color>[
+                  Color(0xFF80DEEA),
+                  Color(0xFFE0F7FA),
+                  Color(0xFF26C6DA),
+                ],
+              ),
             ),
-
           ),
         ),
-      ],
+        drawer: Drawer(),
+
+        body:  SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 900,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('images/1.jpg'),
+                          fit: BoxFit.fill
+                      )
+                  ),
+
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        height:210.0,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage('images/pic3.png',),
+                                fit: BoxFit.fill
+                            )
+                        ),
+
+                      ),
+
+                      Positioned(
+                        left: 30,
+                        top: 50,
+                        child: Container(
+                          height: 550,
+                          width: 400,
+                          margin: EdgeInsets.symmetric(horizontal: 20),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            boxShadow:[
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.3),
+                                blurRadius: 10,
+                                spreadRadius: 5,
+                              ),
+                            ],
+
+                          ),
+                          child: Column( children:[
+                            Container(height:230.0,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage('images/pic5.jpg',),
+                                      fit: BoxFit.fill
+                                  )
+                              ),
+                            ),
+
+                          ],
+                          ),
+
+
+                        ),
+
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
-    ],
-    ),
-    ),
-    ),
-      ),
     );
-
   }
 }
+
