ee894fdd048b3bd79096bbf1cef6c47761320b99
QuratUlAinSjaid <80091216+QuratulainSajid712@users.noreply.github.com>
2021-04-02 11:55:14 +0500

    added background color

--- dice_roll_app/lib/main.dart
+++ dice_roll_app/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.amberAccent,
+    ),
+  ));
 }
 
