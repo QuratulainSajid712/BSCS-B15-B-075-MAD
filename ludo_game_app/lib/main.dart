62f4834c6a1c08bec7e0f8605cfcaa390b082bb9
QuratUlAinSjaid <80091216+QuratulainSajid712@users.noreply.github.com>
2021-04-03 22:09:57 +0500

    set background color and initialize page

--- ludo_game_app/lib/main.dart
+++ ludo_game_app/lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.amber,
+      appBar: AppBar(
+        title: Text('Dicee'),
+        backgroundColor: Colors.amberAccent,
+      ),
+      body: LudoPage(),
+    ),
+  )););
 }
 
