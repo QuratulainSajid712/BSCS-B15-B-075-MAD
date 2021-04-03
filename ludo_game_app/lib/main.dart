fb9206bd1ad1433efad4cfe67ce61fcbc3a997e1
QuratUlAinSjaid <80091216+QuratulainSajid712@users.noreply.github.com>
2021-04-03 22:16:47 +0500

    added flat button for Dice1

--- ludo_game_app/lib/main.dart
+++ ludo_game_app/lib/main.dart
@@ -18,9 +18,22 @@ class LudoPage extends StatefulWidget {
 }
 
 class _LudoPageState extends State<LudoPage>{
+  left_image=1;
   @override
   Widget build(BuildContext context) {
-
+    Center(
+        child: Row(
+            children: [
+        Expanded(
+        child: FlatButton(
+        onPressed: () {
+    setState(() {
+    left_image = Random().nextInt(6)+1;
+    });
+    print('Dice1_Value$left_image');
+    },
+        child: Image.asset('images/dice$left_image.png'))
+    ),
   }
 }
 
