fe9c5b9f241e7a7db648fe95b1de940e6a45133c
QuratUlAinSjaid <80091216+QuratulainSajid712@users.noreply.github.com>
2021-04-10 18:13:07 +0500

    changed create button function

--- xylophone_app/lib/main.dart
+++ xylophone_app/lib/main.dart
@@ -12,11 +12,12 @@ class XylophoneApp extends StatelessWidget {
     final play=AudioCache();
     play.play('note$soundnumber.wav');
   }
-  void CreateButon(){
-    Expanded(child:   FlatButton(
-      color: Colors.blue,
+  Expanded CreateButon(Color color, int soundnumber){
+    return Expanded(
+      child:   FlatButton(
+      color: color,
       onPressed: (){
-        playsound(1);
+        playsound(soundnumber);
       },
     ),
     );
@@ -29,50 +30,13 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children:[
-             Expanded(child:   FlatButton(
-               color: Colors.blue,
-               onPressed: (){
-                 playsound(1);
-               },
-             ),),
-        Expanded(
-              child: FlatButton(
-                color: Colors.yellow,
-                onPressed: (){
-                 playsound(2);
-                },
-              ),),
-        Expanded(child: FlatButton(
-                color: Colors.red,
-                onPressed: (){
-                  playsound(3);
-                },
-              ),),
-              Expanded(child: FlatButton(
-                color: Colors.orange,
-                onPressed: (){
-                 playsound(4);
-                },
-              ),),
-             Expanded(child: FlatButton(
-                color: Colors.purple,
-                onPressed: (){
-                  playsound(5);
-                },
-
-              ),),
-              Expanded(child: FlatButton(
-                color: Colors.lightGreenAccent,
-                onPressed: (){
-                playsound(6);
-                },
-              ),),
-              Expanded(child: FlatButton(
-                color: Colors.pinkAccent,
-                onPressed: (){
-                 playsound(7);
-                },
-              ),),
+              CreateButon(Colors.blue, 1),
+              CreateButon(Colors.redAccent, 2),
+              CreateButon(Colors.lightGreenAccent, 3),
+              CreateButon(Colors.orangeAccent, 4),
+              CreateButon(Colors.yellow, 5),
+              CreateButon(Colors.purpleAccent, 6),
+              CreateButon(Colors.greenAccent, 7),
             ],
 
           ),
