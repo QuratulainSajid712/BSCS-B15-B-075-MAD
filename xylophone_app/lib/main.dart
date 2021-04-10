236d929e28c9a3464a4fc2fe78f4dfdfb5a45042
QuratUlAinSjaid <80091216+QuratulainSajid712@users.noreply.github.com>
2021-04-10 17:01:46 +0500

    created xylophone class

--- xylophone_app/lib/main.dart
+++ xylophone_app/lib/main.dart
@@ -3,6 +3,20 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:audioplayers/audio_cache.dart';
 
 void main() {
-  runApp();
+  runApp(XylophoneApp());
 }
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
+
 
