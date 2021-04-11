1b12cd0e06577160a9d9f631978cbebcbfc83ee4
QuratUlAinSjaid <80091216+QuratulainSajid712@users.noreply.github.com>
2021-04-11 13:02:36 +0500

    removed debug tag

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -11,6 +11,7 @@ class QuizApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
         body: SafeArea(
