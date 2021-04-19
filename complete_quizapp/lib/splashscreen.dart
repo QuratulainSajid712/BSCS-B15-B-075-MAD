2e0aa24e4efaefc217db03a9f716b636736b362b
QuratUlAinSjaid <80091216+QuratulainSajid712@users.noreply.github.com>
2021-04-19 11:33:49 +0500

    added stateful class

--- complete_quizapp/lib/splashscreen.dart
+++ complete_quizapp/lib/splashscreen.dart
@@ -1,1 +1,22 @@
 import 'package:flutter/material.dart';
+
+class splashscreen extends StatefulWidget {
+  @override
+  _splashscreenState createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+
+  @override
+  void initState(){
+    super.initState();
+    Timer(Duration(seconds: 3), (){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => homepage(),
+      ));
+    });
+  }
+
+  // added test yourself
+  // and made the text to align at center
+}
