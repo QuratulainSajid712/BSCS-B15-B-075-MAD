2dc071a81cd9febb62d4f4b1ced8c35acc78a9e9
QuratUlAinSjaid <80091216+QuratulainSajid712@users.noreply.github.com>
2021-04-19 15:01:55 +0500

    added collapsing animation

--- complete_quizapp/lib/navigation_drawer.dart
+++ complete_quizapp/lib/navigation_drawer.dart
@@ -1,4 +1,4 @@
-export 'commons/collapsing_list_tile_widget.dart';
+export 'commons/collapse_list_tile.dart';
 export 'commons/collapsing_navigation_drawer_widget.dart';
 export 'navigator/custom_navigation.dart';
 export 'theme.dart';
