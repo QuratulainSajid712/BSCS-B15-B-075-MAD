09c759932c9e71f553260cfb4ad22071958c47d0
QuratUlAinSjaid <80091216+QuratulainSajid712@users.noreply.github.com>
2021-05-01 23:36:22 +0500

    part2 climate App

--- weather_app/lib/climatefile.dart
+++ weather_app/lib/climatefile.dart
@@ -8,6 +8,17 @@ class Climate extends StatefulWidget {
 class _ClimateState extends State<Climate> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ClimateApp'),
+        backgroundColor: Colors.red,
+        actions: [
+          IconButton(icon: Icon(Icons.menu),
+              onPressed: ()=>print('Clicked'),
+          )
+        ],
+
+      ),
+    );
   }
 }
