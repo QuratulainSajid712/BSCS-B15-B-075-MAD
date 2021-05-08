fe0cce1c535c7f7c588cc9c1877e45a5a3e782a7
QuratUlAinSjaid <80091216+QuratulainSajid712@users.noreply.github.com>
2021-05-08 16:24:01 +0500

    aplash screen with animation part 1

--- tictactoe_app_sessional2/lib/Splashscreen/splashscreen.dart
+++ tictactoe_app_sessional2/lib/Splashscreen/splashscreen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:tictactoe_app_sessional2/homepage.dart';
 
 class splashscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:AnimatedSplashScreen(
-        splash:Image.asset('images/tictactoe.jpg'),
+    return Scaffold(
+      body:Center(
+        child:AnimatedSplashScreen(
+          splash: Image.asset('images/tic4.png',cacheWidth: 500, cacheHeight: 400,),
+          nextScreen: Homepage(),
+          splashTransition: SplashTransition.slideTransition,
+          backgroundColor: Colors.lightBlueAccent,
+          duration: 6000,
+        ),
       ),
     );
   }
