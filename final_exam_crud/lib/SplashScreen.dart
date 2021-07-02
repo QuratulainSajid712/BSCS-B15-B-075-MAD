cc8a232cfa58447ebdcf140328e52ee621db7eb5
QuratUlAinSjaid <80091216+QuratulainSajid712@users.noreply.github.com>
2021-07-02 09:49:47 +0500

    added splash screen

---
+++ final_exam_crud/lib/SplashScreen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import 'dart:async';
+
+
+class splashscreen extends StatefulWidget {
+  @override
+  _splashscreenState createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+
+  @override
+  void initState(){
+    super.initState();
+    Timer(Duration(seconds: 10), (){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => homepage(),
+      ));
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo,
+      body: Center(
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundImage:AssetImage('images/quiz.png') ,
+              backgroundColor: Colors.white,
+              radius: 100.0,
+            ),
+            Text(
+              'Qurat-Ul-Ain Sajid',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+
+              ),
+            ),
+            Text(
+              'CIIT/FA17-BCS-075/VHR',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 15.0,
+                letterSpacing: 4.0,
+
+              ),
+            ),
+
+            SizedBox(
+              height: 20,
+            ),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            )
+            
+          ],
+        ),
+      ),
+    );
+  }
+
+
+}
---
+++ final_exam_crud/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'SplashScreen.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Quiz App",
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: splashscreen(),
+    );
+  }
+}
